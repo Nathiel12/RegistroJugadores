ca5e20f2b930dfb2de907a369bae3a83f569107a
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-09-06 13:42:34 -0600

    Creado la interfaz para el repositorio

---
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/Repository/PlayerRepository.kt
@@ -0,0 +1,17 @@
+package edu.ucne.registrojugadores.Domain.Repository
+
+import edu.ucne.registrojugadores.Domain.Model.Player
+import kotlinx.coroutines.flow.Flow
+
+interface PlayerRepository {
+
+    fun observePlayer(): Flow<List<Player>>
+
+    suspend fun getPlayer(id:Int): Player?
+
+    suspend fun upsert(player: Player):Int
+
+    suspend fun delete(id:Int)
+
+    suspend fun getPlayersByName(nombre: String): List<Player>
+}
