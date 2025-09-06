60d173a56fd394b290ad7bbe3e6f55291d94fab1
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-09-06 13:55:16 -0600

    Creado el modulo para la app

---
+++ app/src/main/java/edu/ucne/registrojugadores/RegistroJugadores.kt
@@ -0,0 +1,9 @@
+package edu.ucne.registrojugadores
+
+import android.app.Application
+import androidx.navigation.NavHostController
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroJugadores : Application() {
+}
