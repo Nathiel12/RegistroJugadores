cdc259e121fa8e36848c6f555af5b4b1cc11a05f
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-09-06 13:44:10 -0600

    Creado use case para observar la lista

---
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/UseCase/ObservePlayerUseCase.kt
@@ -0,0 +1,14 @@
+package edu.ucne.registrojugadores.Domain.UseCase
+
+import edu.ucne.registrojugadores.Domain.Model.Player
+import edu.ucne.registrojugadores.Domain.Repository.PlayerRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObservePlayersUseCase @Inject constructor(
+    private val repository: PlayerRepository
+) {
+    operator fun invoke(): Flow<List<Player>> {
+        return repository.observePlayer()
+    }
+}
