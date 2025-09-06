58a5b733837146d2c0df6b41ffd6b03ac9c5730d
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-09-06 13:45:02 -0600

    Creado use case para insertar y actualizar jugadores

---
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/UseCase/UpsertPlayerUseCase.kt
@@ -0,0 +1,28 @@
+package edu.ucne.registrojugadores.Domain.UseCase
+
+import edu.ucne.registrojugadores.Domain.Model.Player
+import edu.ucne.registrojugadores.Domain.Repository.PlayerRepository
+import javax.inject.Inject
+
+class UpsertPlayerUseCase @Inject constructor(
+    private val repository: PlayerRepository
+) {
+    suspend operator fun invoke(player: Player): Result<Int> {
+
+        if (player.Nombres.isBlank()) {
+            return Result.failure(IllegalArgumentException("El nombre no puede estar vacío"))
+        }
+
+        if (player.Nombres.length>50){
+            return Result.failure(IllegalArgumentException("El nombre no puede tener más de 50 caracteres"))
+        }
+
+        if (player.Partidas < 0) {
+            return Result.failure(IllegalArgumentException("Las partidas no pueden ser negativas"))
+        }
+
+        return runCatching {
+            repository.upsert(player)
+        }
+    }
+}
