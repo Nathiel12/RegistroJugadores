a51f330805fba87332407ba6e68bd0ebb147e3a8
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-09-06 13:36:48 -0600

    Creado tabla para base de datos

---
+++ app/src/main/java/edu/ucne/registrojugadores/Data/Local/PlayerEntity.kt
@@ -0,0 +1,12 @@
+package edu.ucne.registrojugadores.Data.Local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Jugadores")
+class PlayerEntity (
+    @PrimaryKey(autoGenerate = true )
+    val Jugadorid: Int=0,
+    val Nombres: String,
+    val Partidas: Int)
+{}
