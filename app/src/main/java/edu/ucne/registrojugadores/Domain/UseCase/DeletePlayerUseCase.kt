a62c94f14016f37aca1035a2a52b3bcca3455cba
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-09-06 13:46:41 -0600

    Creado use case para eliminar jugadores

---
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/UseCase/DeletePlayerUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registrojugadores.Domain.UseCase
+
+import edu.ucne.registrojugadores.Domain.Repository.PlayerRepository
+import javax.inject.Inject
+
+class DeletePlayerUseCase @Inject constructor(
+    private val repository: PlayerRepository
+) {
+    suspend operator fun invoke(id: Int) {
+        if (id <= 0) throw IllegalArgumentException("El ID debe ser mayor que 0")
+        repository.delete(id)
+    }
+}
