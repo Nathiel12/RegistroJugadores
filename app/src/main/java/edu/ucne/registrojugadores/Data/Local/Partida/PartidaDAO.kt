0fcb93f86ab81f09f59b2753b83983cf6883c366
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-09-23 21:18:34 -0600

    Creado nuevo metodo para ver si una partida esta finalizada

--- app/src/main/java/edu/ucne/registrojugadores/Data/Local/Partida/PartidaDAO.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Data/Local/Partida/PartidaDAO.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
+import edu.ucne.registrojugadores.Domain.Model.Partida
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -23,5 +24,8 @@ interface PartidaDAO {
 
     @Query("DELETE FROM partidas WHERE partidaId=:id")
     suspend fun deleteById(id:Int)
+
+    @Query("SELECT * FROM partidas WHERE esFinalizada = 0") // false = en curso
+    fun getPartidasEnCurso(): Flow<List<Partida>>
 
 }
