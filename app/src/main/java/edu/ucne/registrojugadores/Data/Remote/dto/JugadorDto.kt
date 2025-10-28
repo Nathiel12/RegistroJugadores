f4e3c013f4e9bc2e8f26a6b7500f91f030618c1a
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 17:02:27 -0600

    Dividido en Response y Request

--- app/src/main/java/edu/ucne/registrojugadores/Data/Remote/dto/JugadorDto.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Data/Remote/dto/JugadorDto.kt
@@ -1,7 +1,12 @@
 package edu.ucne.registrojugadores.Data.Remote.dto
 
-data class JugadorDto(
+data class JugadorResponse(
     val jugadorId: Int? = null,
+    val nombres: String,
+    val email: String
+)
+
+data class JugadorRequest(
     val nombres: String,
     val email: String
 )
