4825b1089afbe13c6ce7315891fd4ed05b2666ba
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 17:00:17 -0600

    Modificado para usar response y request

--- app/src/main/java/edu/ucne/registrojugadores/Data/Remote/Mapper/JugadorApiMapper.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Data/Remote/Mapper/JugadorApiMapper.kt
@@ -1,16 +1,18 @@
 package edu.ucne.registrojugadores.Data.Remote.Mapper
 
-import edu.ucne.registrojugadores.Data.Remote.dto.JugadorDto
+import edu.ucne.registrojugadores.Data.Remote.dto.JugadorRequest
+import edu.ucne.registrojugadores.Data.Remote.dto.JugadorResponse
 import edu.ucne.registrojugadores.Domain.Model.Player
+import java.util.UUID
 
-fun JugadorDto.toDomain(): Player = Player(
-    Jugadorid = jugadorId ?: 0,
+fun JugadorResponse.toDomain(): Player = Player(
+    id = UUID.randomUUID().toString(),
+    remoteId = jugadorId,
     Nombres = nombres,
     Partidas = 0
 )
 
-fun Player.toDto(): JugadorDto = JugadorDto(
-    jugadorId = if (Jugadorid == 0) null else Jugadorid,
+fun Player.toRequest(): JugadorRequest = JugadorRequest(
     nombres = Nombres,
     email = ""
 )
