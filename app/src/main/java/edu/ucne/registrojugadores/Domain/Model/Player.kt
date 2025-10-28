de894f3719cb8004d88cb6e5498cb5d8252b758b
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 17:10:11 -0600

    Cambiado el id y agregado parametro isPendingCreate

--- app/src/main/java/edu/ucne/registrojugadores/Domain/Model/Player.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/Model/Player.kt
@@ -1,8 +1,12 @@
 package edu.ucne.registrojugadores.Domain.Model
 
+import java.util.UUID
+
 class Player(
-    val Jugadorid: Int=0,
+    val id: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
     val Nombres: String,
-    val Partidas: Int
+    val Partidas: Int,
+    val isPendingCreate: Boolean = false
 ){
 }
