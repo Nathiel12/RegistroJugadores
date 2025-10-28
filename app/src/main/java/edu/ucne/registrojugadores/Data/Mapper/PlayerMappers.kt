83fd548ae12f6c44e87607c48f8bba46c65cac70
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 17:14:26 -0600

    Unificacion de mappers y agregado nuevos mappers para response y request

--- app/src/main/java/edu/ucne/registrojugadores/Data/Mapper/PlayerMappers.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Data/Mapper/PlayerMappers.kt
@@ -1,18 +1,47 @@
+// Data/Mapper.kt
 package edu.ucne.registrojugadores.Data.Mapper
 
 import edu.ucne.registrojugadores.Data.Local.Player.PlayerEntity
+import edu.ucne.registrojugadores.Data.Remote.dto.JugadorRequest
+import edu.ucne.registrojugadores.Data.Remote.dto.JugadorResponse
 import edu.ucne.registrojugadores.Domain.Model.Player
+import java.util.UUID
 
-fun PlayerEntity.toDomain():Player=
-    Player(
-        Jugadorid = Jugadorid,
-        Nombres = Nombres,
-        Partidas = Partidas
-    )
+fun PlayerEntity.toDomain(): Player = Player(
+    id = id,
+    remoteId = remoteId,
+    Nombres = Nombres,
+    Partidas = Partidas
+)
 
-fun Player.toEntity():PlayerEntity=
-    PlayerEntity(
-        Jugadorid = Jugadorid,
-        Nombres = Nombres,
-        Partidas = Partidas
-    )
+fun Player.toEntity(): PlayerEntity = PlayerEntity(
+    id = id,
+    remoteId = remoteId,
+    Nombres = Nombres,
+    Partidas = Partidas,
+    isPendingCreate = false
+)
+
+fun JugadorResponse.toEntity(): PlayerEntity = PlayerEntity(
+    id = UUID.randomUUID().toString(),
+    remoteId = jugadorId,
+    Nombres = nombres,
+    Partidas = 0
+)
+
+fun JugadorResponse.toDomain(): Player = Player(
+    id = UUID.randomUUID().toString(),
+    remoteId = jugadorId,
+    Nombres = nombres,
+    Partidas = 0
+)
+
+fun PlayerEntity.toRequest(): JugadorRequest = JugadorRequest(
+    nombres = this.Nombres,
+    email = ""
+)
+
+fun Player.toRequest(): JugadorRequest = JugadorRequest(
+    nombres = Nombres,
+    email = ""
+)
