134b8c64146e135a7bca15f713857ed62fbfc71b
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 17:15:19 -0600

    Cambiado id a string y agregado dos nuevos metodos

--- app/src/main/java/edu/ucne/registrojugadores/Domain/Repository/PlayerRepository.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/Repository/PlayerRepository.kt
@@ -7,13 +7,19 @@ interface PlayerRepository {
 
     fun observePlayer(): Flow<List<Player>>
 
-    suspend fun getPlayer(id:Int): Player?
+    suspend fun getPlayer(id: String): Player?
 
-    suspend fun upsert(player: Player):Int
+    suspend fun createPlayerLocal(player: Player): String
 
-    suspend fun delete(id:Int)
+    suspend fun upsert(player: Player)
+
+    suspend fun delete(id: String)
 
     suspend fun getPlayersByName(nombre: String): List<Player>
 
     suspend fun getAllPlayers(): List<Player>
+
+    suspend fun postPendingPlayers(): Boolean
+
+    suspend fun descargarJugadoresDeApi(): Boolean
 }
