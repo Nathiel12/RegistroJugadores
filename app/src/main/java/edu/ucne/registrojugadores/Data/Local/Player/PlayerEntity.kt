8337d6ecea875de28730a4f65bac273b34d3f71b
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 17:13:21 -0600

    Cambiado el id y agregado parametro

--- app/src/main/java/edu/ucne/registrojugadores/Data/Local/Player/PlayerEntity.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Data/Local/Player/PlayerEntity.kt
@@ -2,11 +2,15 @@ package edu.ucne.registrojugadores.Data.Local.Player
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity(tableName = "Jugadores")
-class PlayerEntity (
-    @PrimaryKey(autoGenerate = true )
-    val Jugadorid: Int=0,
+data class PlayerEntity (
+    @PrimaryKey
+    val id: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
     val Nombres: String,
-    val Partidas: Int)
+    val Partidas: Int,
+    val isPendingCreate: Boolean = false
+)
 {}
