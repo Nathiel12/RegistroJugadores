2422b00ebaa94f2904f4feb8f7e5adb5d2471e99
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 16:43:47 -0600

    Cambiado manejo de errores

--- app/src/main/java/edu/ucne/registrojugadores/Domain/UseCase/PlayersUseCase/UpsertPlayerUseCase.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/UseCase/PlayersUseCase/UpsertPlayerUseCase.kt
@@ -7,22 +7,19 @@ import javax.inject.Inject
 class UpsertPlayerUseCase @Inject constructor(
     private val repository: PlayerRepository
 ) {
-    suspend operator fun invoke(player: Player): Result<Int> {
-
+    suspend operator fun invoke(player: Player) {
         if (player.Nombres.isBlank()) {
-            return Result.failure(IllegalArgumentException("El nombre no puede estar vacío"))
+            throw IllegalArgumentException("El nombre no puede estar vacío")
         }
 
-        if (player.Nombres.length>50){
-            return Result.failure(IllegalArgumentException("El nombre no puede tener más de 50 caracteres"))
+        if (player.Nombres.length > 50) {
+            throw IllegalArgumentException("El nombre no puede tener más de 50 caracteres")
         }
 
         if (player.Partidas < 0) {
-            return Result.failure(IllegalArgumentException("Las partidas no pueden ser negativas"))
+            throw IllegalArgumentException("Las partidas no pueden ser negativas")
         }
 
-        return runCatching {
-            repository.upsert(player)
-        }
+        repository.upsert(player)
     }
 }
