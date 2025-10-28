968d3e0e1c74ad4dde34dbc130ca26732d989515
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-28 17:06:36 -0600

    Modificado para usar id y agregado dos parametros nuevos

--- app/src/main/java/edu/ucne/registrojugadores/Presentation/Players/List/ListPlayerUiEvent.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Presentation/Players/List/ListPlayerUiEvent.kt
@@ -2,8 +2,10 @@ package edu.ucne.registrojugadores.Presentation.Players.List
 
 sealed interface ListPlayerUiEvent{
     data object Load: ListPlayerUiEvent
-    data class Delete(val id: Int) : ListPlayerUiEvent
+    data class Delete(val id: String) : ListPlayerUiEvent
     data object CreateNew: ListPlayerUiEvent
-    data class Edit(val id: Int) : ListPlayerUiEvent
+    data class Edit(val id: String) : ListPlayerUiEvent
     data class ShowMessage(val message: String) : ListPlayerUiEvent
+    data object SyncPending : ListPlayerUiEvent
+    data object DownloadFromApi : ListPlayerUiEvent
 }
